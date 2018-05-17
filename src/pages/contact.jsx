a44eb3e42adb1ac47cc7b003fd03d903101819f6
Timo Horstschäfer <mail@timo-horstschaefer.de>
2018-05-17 08:32:08 +0200

    remove contact form to reduce spam

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -44,91 +44,52 @@ class Map extends React.Component<{}> {
   }
 }
 
-const IndexPage = (props: Object) => {
-  const { i18n } = props;
-  return (
-    <div>
-      <Header {...props} />
+const IndexPage = (props: Object) => (
+  <div>
+    <Header {...props} />
 
-      <main className="main-content">
+    <main className="main-content">
 
-        <div className="section">
-          <div className="container">
+      <div className="section">
+        <div className="container">
 
-            <div className="row gap-y">
-              <div className="col-12 col-xl-6">
+          <div className="bg-gray h-full p-5 imprint">
 
-                <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                  <input type="hidden" name="form-name" value="contact" />
-                  <div className="form-group">
-                    <input className="form-control form-control-lg" type="text" name="name" placeholder={i18n.t`Your name`} />
-                  </div>
-
-                  <div className="form-group">
-                    <input className="form-control form-control-lg" type="email" name="email" placeholder={i18n.t`Your email`} />
-                  </div>
-
-                  <div className="form-group">
-                    <textarea className="form-control form-control-lg" name="message" rows="4" placeholder={i18n.t`Your message`} />
-                  </div>
-
-                  <div className="form-group d-none">
-                    <label htmlFor="bot-field">Don’t fill this out if you’re human: <input name="bot-field" /></label>
-                  </div>
-
-                  <button className="btn btn-lg btn-primary btn-block" type="submit">{i18n.t`Send message`}</button>
-                </form>
-
+            <div className="row">
+              <div className="col-md-6">
+                <p>
+                  <h6>Ledgy AG</h6>
+                    Forchstrasse 60<br />
+                    8008 Zürich<br />
+                  <Trans>Switzerland</Trans>
+                </p>
+                <p>
+                  <h6><Trans>VAT number</Trans></h6>
+                    CHE‑261.454.963 MWST
+                </p>
               </div>
-
-
-              <div className="col-12 col-xl-6">
-                <div className="bg-gray h-full p-5 imprint">
-
-                  <h5><Trans>Imprint</Trans></h5>
-
-                  <div className="row">
-                    <div className="col-md-6">
-
-                      <p>
-                        <h6>Ledgy AG</h6>
-                        Forchstrasse 60<br />
-                        8008 Zürich<br />
-                        <Trans>Switzerland</Trans>
-                      </p>
-
-                      <p>
-                        <h6><Trans>Contact</Trans></h6>
-                        <a href="mailto:contact@ledgy.com">contact@ledgy.com</a><br />
+              <div className="col-md-6">
+                <p>
+                  <h6><Trans>Contact</Trans></h6>
+                  <a href="mailto:contact@ledgy.com">contact@ledgy.com</a><br />
                         +41 (0) 44 585 21 23
-                      </p>
-                    </div>
-                    <div className="col-md-6">
-                      <p>
-                        <h6><Trans>VAT number</Trans></h6>
-                        CHE‑261.454.963 MWST
-                      </p>
-                      <p>
-                        <h6><Trans>Managing directors</Trans></h6>
-                        Yoko Spirig<br />
-                        Ben-Elias Brandt<br />
-                        Timo Horstschäfer
-                      </p>
-                    </div>
-                  </div>
-
-                </div>
+                </p>
+                <p>
+                  <h6><Trans>Managing directors</Trans></h6>
+                    Yoko Spirig<br />
+                    Ben-Elias Brandt<br />
+                    Timo Horstschäfer
+                </p>
               </div>
             </div>
 
-
           </div>
         </div>
+      </div>
 
-        <Map />
-      </main>
-    </div>
-  );
-};
+      <Map />
+    </main>
+  </div>
+);
 
 export default withI18n()(IndexPage);
