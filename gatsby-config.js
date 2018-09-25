1499117480d8a912b17b9544b1ee824a320b9353
Timo Horstschäfer <mail@timo-horstschaefer.de>
2018-09-25 20:52:58 +0200

    fix offline?

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,10 +42,7 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: 'gatsby-plugin-offline',
-      options: { navigateFallbackWhitelist: [] },
-    },
+    'gatsby-plugin-offline',
     {
       resolve: 'gatsby-plugin-netlify',
       options: {
