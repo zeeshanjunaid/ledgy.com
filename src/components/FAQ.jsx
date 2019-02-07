95675c4c6837d1272d090bddce4bbbab29a9d486
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-02-08 00:29:23 +0100

    open on anchor

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -8,15 +8,18 @@ export const FAQ = ({ id, question, children }: {|
   id: string,
   question: string,
   children: React.Node
-|}) => (
-  <div className="card mb-0">
-    <h6 className="card-title mt-0 bg-white" id={id}>
-      <a className="collapsed" data-toggle="collapse" href={`#${id}-body`}>{question}</a>
-    </h6>
-    <div id={`${id}-body`} className="collapse">
-      <div className="card-body py-1">
-        {children}
+|}) => {
+  const open = window.top.location.hash === `#${id}`;
+  return (
+    <div className="card mb-0">
+      <h6 className="card-title mt-0 bg-white" id={id}>
+        <a className={open ? 'collapse' : ''} data-toggle="collapse" href={`#${id}-body`}>{question}</a>
+      </h6>
+      <div id={`${id}-body`} className={`collapse ${open ? 'show' : ''}`}>
+        <div className="card-body py-1">
+          {children}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
