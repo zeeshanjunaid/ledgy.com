ec8679cbc2b4ec0bc502640292dd9a00dd0aa147
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-02-08 16:16:36 +0100

    fix classes

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -9,13 +9,13 @@ export const FAQ = ({ id, question, children }: {|
   question: string,
   children: React.Node
 |}) => {
-  const open = typeof window !== 'undefined' && window.top.location.hash === `#${id}`;
+  const isOpen = typeof window !== 'undefined' && window.top.location.hash === `#${id}`;
   return (
     <div className="card mb-0">
       <h6 className="card-title mt-0 bg-white" id={id}>
-        <a className={open ? 'collapse' : ''} data-toggle="collapse" href={`#${id}-body`}>{question}</a>
+        <a className={!isOpen ? 'collapsed' : ''} data-toggle="collapse" href={`#${id}-body`}>{question}</a>
       </h6>
-      <div id={`${id}-body`} className={`collapse ${open ? 'show' : ''}`}>
+      <div id={`${id}-body`} className={`collapse ${isOpen ? 'show' : ''}`}>
         <div className="card-body py-1">
           {children}
         </div>
