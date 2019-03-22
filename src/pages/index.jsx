136b1c89fdadfb99f8179f00827f9cdf4c48c0d2
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-03-22 17:18:19 +0100

    animate tablet on scroll

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,48 +9,64 @@ import { FeatureLinks } from '../components/Features';
 import SecurityRow from '../components/SecurityRow';
 import { demoUrl, targetBlank } from '../layouts/utils';
 
-const Header = ({ i18n, data }: Props) => (
-  <header className="header bg-ledgy main-banner">
-    <div className="container">
-      <div className="text-white text-center mb-1 mb-sm-3 mb-lg-7">
-        <h1>
-          <Trans>The Smart Equity Management Tool</Trans>
-        </h1>
+const Header = ({ i18n, data }: Props) => {
+  let scrolling = false;
+  window.onscroll = () => {
+    scrolling = true;
+  };
+  setInterval(() => {
+    if (scrolling) {
+      scrolling = false;
+      const tablet = document.getElementById('tablet-ledgy');
+      const banner = document.querySelector('header');
+      const { scrollY } = window;
+      if (tablet && scrollY <= banner.clientHeight) {
+        tablet.style.transform = `rotateZ(${scrollY / 50}deg) translateY(${scrollY /
+          25}%) skew(-${scrollY / 100}deg)`;
+      }
+    }
+  }, 50);
+  return (
+    <header className="header bg-ledgy main-banner">
+      <div id="tablet-ledgy" data-aos="fade-up">
+        <Img {...data.laptop} alt={i18n.t`Screenshot of the Ledgy app`} />
       </div>
-      <div className="row gap-y mt-5 pb-4 pb-md-7">
-        <div className="col-lg-6 ml-auto mb-4 mb-lg-0">
-          <div className="text-white">
-            <p>
-              <Trans>
-                Stay on top of your vesting schedules, options, phantom plans, and convertible
-                loans. Get fast insights for financing rounds or exit negotiations using our
-                built-in modeling tools.
-                <br />
-                <br />
-                Are you an investor or employee? With the portfolio you will always have the latest
-                information about your investment and vesting at your fingertips.
-              </Trans>
-            </p>
-          </div>
-          <a
-            className="btn btn-block d-sm-inline btn-xl mx-1 btn-round btn-outline-light"
-            href={demoUrl}
-            {...targetBlank}
-          >
-            <Trans>See the demo</Trans>
-          </a>
-          <a className="btn btn-block d-sm-inline btn-xl mx-1 btn-round btn-light" href="#try">
-            <Trans>Get Started</Trans>
-          </a>
-        </div>
+      <div className="container">
+        <div className="row gap-y mt-2 pb-4 pb-md-6">
+          <div className="col-lg-6 mr-auto mb-4 mb-lg-0">
+            <h1 className="text-white text-center text-md-left mb-1 mb-sm-3 mb-lg-6">
+              <Trans>The Smart Equity Management Tool</Trans>
+            </h1>
 
-        <div className="col-lg-6 order-lg-first" data-aos="fade-up">
-          <Img {...data.laptop} alt={i18n.t`Screenshot of the Ledgy app`} />
+            <div className="text-white pb-4">
+              <p>
+                <Trans>
+                  Stay on top of your vesting schedules, options, phantom plans, and convertible
+                  loans. Get fast insights for financing rounds or exit negotiations using our
+                  built-in modeling tools.
+                  <br />
+                  <br />
+                  Are you an investor or employee? With the portfolio you will always have the
+                  latest information about your investment and vesting at your fingertips.
+                </Trans>
+              </p>
+            </div>
+            <a
+              className="btn btn-block d-sm-inline btn-xl mx-1 btn-round btn-outline-light"
+              href={demoUrl}
+              {...targetBlank}
+            >
+              <Trans>See the demo</Trans>
+            </a>
+            <a className="btn btn-block d-sm-inline btn-xl mx-1 btn-round btn-light" href="#try">
+              <Trans>Get Started</Trans>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 const Reference = ({ img, name }: { img: Object, name: string }) => (
   <div style={{ width: '200px' }} className="my-4">
@@ -121,7 +137,7 @@ export const pageQuery = graphql`
   query {
     ...FeaturesFragment
 
-    laptop: imageSharp(fluid: { originalName: { regex: "/laptop.png/" } }) {
+    laptop: imageSharp(fluid: { originalName: { regex: "/ipad.png/" } }) {
       fluid(maxWidth: 2000) {
         ...GatsbyImageSharpFluid_noBase64
       }
