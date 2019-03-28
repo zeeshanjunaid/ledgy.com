fd8d3a45d31b331c9cac7e4ec175f781d05c3b28
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-03-28 18:38:23 +0100

    fix word break in header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,7 +16,7 @@ const Header = ({ i18n, data }: Props) => {
         <div className="row gap-y mt-md-2 pb-4 pb-md-6">
           <div className="col-lg-6">
             <h1 className="text-white mb-2 mb-sm-3">
-              <Trans>The New Standard in Equity Management</Trans>
+              <Trans>The New Standard in Equity Management</Trans>
             </h1>
             <h5 className="text-white font-weight-light pb-4 pb-lg-6 mb-0">
               <Trans>Made for startups, great for investors</Trans>
