5b222647790d7dd6d8a9bf4b8abb078e749ce48e
Ben <ben@ledgy.com>
2019-04-04 23:19:14 +0200

    last mini fixes

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -123,7 +123,7 @@ const IndexPage = ({ i18n, ...props }: Props) => (
             />
             <PrivacyElement
               icon={faChartPie}
-              title={i18n.t`Company information`}
+              title={i18n.t`Equity data`}
               body={i18n.t`Nobody has any access to the equity data you provide. They are safely stored at a secure data center in France. Your stakeholders have no access and don’t get emails before you explicitly invite them`}
               size="6"
             />
