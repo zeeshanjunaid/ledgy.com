49977ee8aacc8e25d27b9e0b9824bf6f0693ab0a
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-04-24 23:03:06 +0200

    fix features on index

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -91,12 +91,14 @@ export const FeatureLinks = ({
       </h2>
     </header>
 
-    <div className="row gap-y justify-content-center">
-      <FeatureLink {...props} name={i18n.t`Employee Participation Plans`} url="esop" />
-      <FeatureLink {...props} name={i18n.t`Cap Table`} url="captable" />
-      <FeatureLink {...props} name={i18n.t`Modeling`} url="modeling" />
-      <FeatureLink {...props} name={i18n.t`Investor Portfolio`} url="portfolio" />
-      <FeatureLink {...props} name={i18n.t`Investor Relations`} url="relations" />
+    <div className="container custom-container">
+      <div className="row gap-y justify-content-center">
+        <FeatureLink {...props} name={i18n.t`Employee Participation Plans`} url="esop" />
+        <FeatureLink {...props} name={i18n.t`Cap Table`} url="captable" />
+        <FeatureLink {...props} name={i18n.t`Modeling`} url="modeling" />
+        <FeatureLink {...props} name={i18n.t`Investor Portfolio`} url="portfolio" />
+        <FeatureLink {...props} name={i18n.t`Investor Relations`} url="relations" />
+      </div>
     </div>
   </div>
 );
