75a6dba2e157210e5de1a1262c625888406ea5b5
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-05-03 16:59:45 +0200

    delete unnecessary

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,19 +38,17 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage, createRedirect } = actions;
-  [['/jobs/', '/jobs/software-engineer/', '/new-features/kpis-and-reports']].forEach(
-    ([fromPath, toPath]) => {
-      const redirectInBrowser = true;
-      createRedirect({ fromPath, toPath, redirectInBrowser });
-      languages.forEach(lang =>
-        createRedirect({
-          fromPath: `/${lang}${fromPath}`,
-          toPath: `/${lang}${toPath}`,
-          redirectInBrowser
-        })
-      );
-    }
-  );
+  [['/jobs/', '/jobs/software-engineer/']].forEach(([fromPath, toPath]) => {
+    const redirectInBrowser = true;
+    createRedirect({ fromPath, toPath, redirectInBrowser });
+    languages.forEach(lang =>
+      createRedirect({
+        fromPath: `/${lang}${fromPath}`,
+        toPath: `/${lang}${toPath}`,
+        redirectInBrowser
+      })
+    );
+  });
 
   const component = path.resolve('./src/layouts/markdown.jsx');
   return new Promise((resolve, reject) => {
