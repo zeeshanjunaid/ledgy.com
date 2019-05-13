f87c5aa4f04a9195251e0ca08ba374563b4551a1
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-05-13 16:40:09 +0200

    update collaboration page

--- src/pages/features/collaboration.jsx
+++ src/pages/features/collaboration.jsx
@@ -6,7 +6,7 @@ import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 
 import { FeatureLinks } from '../../components/Feature';
-import { Title, FeatureLi, Hr, ChevronRight } from '../../layouts/utils';
+import { Title, FeatureList, FeatureLi, Hr, ChevronRight } from '../../layouts/utils';
 
 export default withI18n()(({ i18n, ...props }: Props) => (
   <div>
@@ -58,155 +58,102 @@ export default withI18n()(({ i18n, ...props }: Props) => (
 
           <Hr />
 
-          <div className="row align-items-center my-8 pb-2">
-            <div className="col-md-5 mr-auto">
-              <h2>
-                <Trans>Granular access rights</Trans>
-              </h2>
-
-              <ul className="pl-0 pt-2">
-                <FeatureLi>
-                  <Trans>
-                    Stakeholders will only be notified if you explicitely invite them yourself
-                  </Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    <strong>Admin</strong> Full control over the company for your co-founders
-                  </Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    <strong>View</strong> See all cap table info in a read-only mode for due
-                    diligences
-                  </Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    <strong>Portfolio</strong> See only the own stake in their{' '}
-                    <Link href to={`${props.prefix}/features/investors/`}>
-                      portfolio
-                      <ChevronRight />
-                    </Link>
-                  </Trans>
-                </FeatureLi>
-              </ul>
-            </div>
-
-            <div className="col-md-7" data-aos="fade-left">
-              <Img {...props.data.accessRights} alt={i18n.t`Stakeholders access rights`} />
-            </div>
-          </div>
+          <FeatureList
+            textSize="5"
+            header={<Trans>Granular access rights</Trans>}
+            features={[
+              <Trans>
+                Stakeholders will only be notified if you explicitely invite them yourself
+              </Trans>,
+              <Trans>
+                <strong>Admin</strong> Full control over the company for your co-founders
+              </Trans>,
+              <Trans>
+                <strong>View</strong> See all cap table info in a read-only mode for due diligences
+              </Trans>,
+              <Trans>
+                <strong>Portfolio</strong> See only the own stake in their{' '}
+                <Link href to={`${props.prefix}/features/investors/`}>
+                  portfolio
+                  <ChevronRight />
+                </Link>
+              </Trans>
+            ]}
+            imgSize="7"
+            img={<Img {...props.data.accessRights} alt={i18n.t`Stakeholders access rights`} />}
+          />
 
           <Hr />
 
-          <div className="row align-items-center my-8 pb-2">
-            <div className="col-md-5 ml-auto">
-              <h2>
-                <Trans>Due diligence history</Trans>
-              </h2>
-              <ul className="pl-0 pt-2">
-                <FeatureLi>
-                  <Trans>
-                    The transaction based cap table allows browsing through the history and sets it
-                    up for future due diligences, saving you hours of work and lawyer costs
-                  </Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    Track your valuations and understand the history in terms of percentage and
-                    number of shares
-                  </Trans>
-                </FeatureLi>
-              </ul>
-            </div>
-
-            <div className="col-md-7 order-md-first" data-aos="fade-right">
-              <Img {...props.data.valuation} alt={i18n.t`Valuation`} />
-            </div>
-          </div>
+          <FeatureList
+            textSize="5"
+            header={<Trans>Due diligence history</Trans>}
+            features={[
+              <Trans>
+                The transaction-based cap table allows browsing through the history and sets it up
+                for future due diligences, saving you hours of work and lawyer costs
+              </Trans>,
+              <Trans>
+                Track your valuations and understand the history in terms of percentage and number
+                of shares
+              </Trans>
+            ]}
+            imgSize="7"
+            img={<Img {...props.data.valuation} alt={i18n.t`Valuation`} />}
+            imgFirst
+          />
 
           <Hr />
 
-          <div className="row align-items-center pb-7">
-            <div className="col-md-5 ml-auto">
-              <h2>
-                <Trans>Data room</Trans>
-              </h2>
-              <ul className="pl-0 pt-3">
-                <FeatureLi>
-                  <Trans>Secure legal document storage</Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>Prove your transactions by linking their respective legal documents</Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    Make linked documents accessible to stakeholders involved in the transaction
-                  </Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    Quickly pull up documents by the information of their linked transactions, for
-                    example all of a specific stakeholder, share class or date range
-                  </Trans>
-                </FeatureLi>
-              </ul>
-            </div>
-
-            <div className="col-md-7" data-aos="fade-left">
-              <Img {...props.data.documentManagement} alt={i18n.t`Document management`} />
-            </div>
-          </div>
+          <FeatureList
+            textSize="5"
+            header={<Trans>Data room</Trans>}
+            features={[
+              <Trans>Secure legal document storage</Trans>,
+              <Trans>Prove your transactions by linking their respective legal documents</Trans>,
+              <Trans>
+                Make linked documents accessible to stakeholders involved in the transaction
+              </Trans>,
+              <Trans>
+                Quickly pull up documents by the information of their linked transactions; for
+                example all of a specific stakeholder, share class or date range
+              </Trans>
+            ]}
+            imgSize="7"
+            img={<Img {...props.data.documentManagement} alt={i18n.t`Document management`} />}
+          />
 
           <Hr />
 
-          <div className="row align-items-center pb-7">
-            <div className="col-md-5 ml-auto">
-              <h2>
-                <Trans>Edit log</Trans>
-              </h2>
-              <ul className="pl-0 pt-3">
-                <FeatureLi>
-                  <Trans>Keep track of who edited what</Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    Equity data is too important to be accidentally tweaked in a spreadsheet
-                  </Trans>
-                </FeatureLi>
-              </ul>
-            </div>
-
-            <div className="col-md-7 order-md-first" data-aos="fade-right">
-              <Img {...props.data.editLog} alt={i18n.t`Record of edits`} />
-            </div>
-          </div>
+          <FeatureList
+            textSize="6"
+            header={<Trans>Edit log</Trans>}
+            features={[
+              <Trans>Keep track of who edited what</Trans>,
+              <Trans>
+                Equity data is too important to be accidentally tweaked in a spreadsheet
+              </Trans>
+            ]}
+            imgSize="5"
+            img={<Img {...props.data.editLog} alt={i18n.t`Record of edits`} />}
+            imgFirst
+          />
 
           <Hr />
 
-          <div className="row align-items-center pb-7">
-            <div className="col-md-5 ml-auto">
-              <h2>
-                <Trans>Holding confirmations</Trans>
-              </h2>
-              <ul className="pl-0 pt-3">
-                <FeatureLi>
-                  <Trans>Download holding confirmations with a single click</Trans>
-                </FeatureLi>
-                <FeatureLi>
-                  <Trans>
-                    They will list all assets owned by the stakeholder and optionally include a tax
-                    value of the shares
-                  </Trans>
-                </FeatureLi>
-              </ul>
-            </div>
-
-            <div className="col-md-7" data-aos="fade-left">
-              <Img {...props.data.holdingConfirmationPdf} alt={i18n.t`Holding confirmation`} />
-            </div>
-          </div>
+          <FeatureList
+            textSize="5"
+            header={<Trans>Holding confirmations</Trans>}
+            features={[
+              <Trans>Download holding confirmations with a single click</Trans>,
+              <Trans>
+                They will list all assets owned by the stakeholder and optionally include a tax
+                value of the shares
+              </Trans>
+            ]}
+            imgSize="7"
+            img={<Img {...props.data.holdingConfirmationPdf} alt={i18n.t`Holding confirmation`} />}
+          />
 
           <FeatureLinks {...props} i18n={i18n} page="collaboration" />
         </div>
@@ -261,7 +208,7 @@ export const pageQuery = graphql`
         ...GatsbyImageSharpFluid
       }
     }
-    editLog: imageSharp(fluid: { originalName: { regex: "/uri.jpg/" } }) {
+    editLog: imageSharp(fluid: { originalName: { regex: "/edit-log.png/" } }) {
       fluid(maxWidth: 800) {
         ...GatsbyImageSharpFluid
       }
