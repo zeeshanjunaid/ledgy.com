e16059527664c7bc72635a586eab77a0bc9eda24
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-05-13 17:54:33 +0200

    and more screenshot updates

--- src/pages/features/esop.jsx
+++ src/pages/features/esop.jsx
@@ -197,7 +197,7 @@ export const pageQuery = graphql`
         ...GatsbyImageSharpFluid
       }
     }
-    notificationEmail: imageSharp(fluid: { originalName: { regex: "/uri.jpg/" } }) {
+    notificationEmail: imageSharp(fluid: { originalName: { regex: "/notification-email.png/" } }) {
       fluid(maxWidth: 800) {
         ...GatsbyImageSharpFluid
       }
