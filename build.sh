1a114e44207ae648e67294d76270a23abb6c3126
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-11-07 18:26:50 +0100

    push new files to loco

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ "$BRANCH" = "master" ]; then
+  curl -u ${LOCO_KEY}: -d @src/locale/en/messages.po https://localise.biz/api/import/po
+fi
+
 curl https://localise.biz/api/export/locale/de.po?key=jnXvFinOCKAyCWlV7BradDB0Vh9RSbjc > src/locale/de/messages.po &&
 curl https://localise.biz/api/export/locale/fr.po?key=jnXvFinOCKAyCWlV7BradDB0Vh9RSbjc > src/locale/fr/messages.po &&
 lingui compile &&
