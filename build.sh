785fa70cc4d21e19dc5e5847baefb80c34bcb509
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-11-11 19:36:08 +0100

    fix scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+./getTranslations.sh
 
 if [ "$BRANCH" = "master" ]; then
   npm run extract
   curl -u ${LOCO_KEY}: --data-binary @src/locale/en/messages.po https://localise.biz/api/import/po
 fi
 
-./getTranslations.sh &&
 gatsby build
--- getTranslations.sh
+++ getTranslations.sh
@@ -2,6 +2,7 @@
 
 getPo () {
   LANG=$1
+  mkdir -p src/locale/${LANG}
   curl https://localise.biz/api/export/locale/${LANG}.po?key=jnXvFinOCKAyCWlV7BradDB0Vh9RSbjc > src/locale/${LANG}/messages.po
 }
 
