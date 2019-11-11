e5e2423c9ec1dad83fcc5e2a887c7b9fafb73803
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-11-11 17:50:15 +0100

    remove po files

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+
 if [ "$BRANCH" = "master" ]; then
+  npm run extract
   curl -u ${LOCO_KEY}: --data-binary @src/locale/en/messages.po https://localise.biz/api/import/po
 fi
 
-curl https://localise.biz/api/export/locale/de.po?key=jnXvFinOCKAyCWlV7BradDB0Vh9RSbjc > src/locale/de/messages.po &&
-curl https://localise.biz/api/export/locale/fr.po?key=jnXvFinOCKAyCWlV7BradDB0Vh9RSbjc > src/locale/fr/messages.po &&
+getPo () {
+  LANG=$1
+  curl https://localise.biz/api/export/locale/${LANG}.po?key=jnXvFinOCKAyCWlV7BradDB0Vh9RSbjc > src/locale/${LANG}/messages.po
+}
+
+getPo en &&
+getPo de &&
+getPo fr &&
 lingui compile &&
 gatsby build
