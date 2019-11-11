01d3e4a238ef663d0d0cbc7f068a5e7cdef4ab6b
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-11-11 17:45:12 +0100

    fix upload encoding

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$BRANCH" = "master" ]; then
-  curl -u ${LOCO_KEY}: -d @src/locale/en/messages.po https://localise.biz/api/import/po
+  curl -u ${LOCO_KEY}: --data-binary @src/locale/en/messages.po https://localise.biz/api/import/po
 fi
 
 curl https://localise.biz/api/export/locale/de.po?key=jnXvFinOCKAyCWlV7BradDB0Vh9RSbjc > src/locale/de/messages.po &&
