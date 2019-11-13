cbfa01ca481f900566f6e81e5a596dcee1756e94
Jahlela Hasle <jahlela5@gmail.com>
2019-11-13 15:19:38 +0000

    Mobile-proof the USPs

--- src/components/partners/UniqueSellingPropositions.jsx
+++ src/components/partners/UniqueSellingPropositions.jsx
@@ -5,6 +5,9 @@ import { Trans } from '@lingui/react';
 import { faCompass, faChartLine, faDollarSign } from '@fortawesome/free-solid-svg-icons';
 import ReadMore from '../ReadMore';
 
+const readMoreWrapperClasses = 'col-10 offset-1 col-smd-8 offset-md-2 col-lg-4 offset-lg-0';
+const ulClasses = 'width-fit-content text-left pb-0 mb-0 mx-auto';
+
 // eslint-disable-next-line import/prefer-default-export
 export const UniqueSellingPropositions = (props: Props) => (
   <div className="container pt-6 pb-4">
@@ -12,12 +15,12 @@ export const UniqueSellingPropositions = (props: Props) => (
       <Trans>The Smoothest Experience for Startups and Lawyers</Trans>
     </h5>
     <div className="row gap-y text-center my-5">
-      <div className="col-md-4">
+      <div className={readMoreWrapperClasses}>
         <ReadMore
           icon={faCompass}
           title="Single source of truth"
           subtitle={
-            <ul className="text-left pb-0 mb-0">
+            <ul className={ulClasses}>
               <li>
                 <Trans>Always up-to-date cap table</Trans>
               </li>
@@ -33,12 +36,12 @@ export const UniqueSellingPropositions = (props: Props) => (
         />
       </div>
 
-      <div className="col-8 col-md-4">
+      <div className={readMoreWrapperClasses}>
         <ReadMore
           icon={faChartLine}
           title="Powerful modeling insights"
           subtitle={
-            <ul className="text-left pb-0 mb-0">
+            <ul className={ulClasses}>
               <li>
                 <Trans>Flexible and accurate fundraising round modeling</Trans>
               </li>
@@ -51,12 +54,12 @@ export const UniqueSellingPropositions = (props: Props) => (
         />
       </div>
 
-      <div className="col-8 col-md-4">
+      <div className={readMoreWrapperClasses}>
         <ReadMore
           icon={faDollarSign}
           title="Faster due diligence"
           subtitle={
-            <ul className="text-left pb-0 mb-0">
+            <ul className={ulClasses}>
               <li>
                 <Trans>Intuitive and searchable history</Trans>
               </li>
