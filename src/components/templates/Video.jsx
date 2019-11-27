0a8e08646d80244ebb87c21690e7b2f8bb350765
Timo Horstschäfer <mail@timo-horstschaefer.de>
2019-11-27 20:54:28 +0100

    update video

--- src/components/templates/Video.jsx
+++ src/components/templates/Video.jsx
@@ -5,7 +5,7 @@ import { Embed } from '../Embed';
 
 export const Video = () => (
   <Embed
-    src="https://www.youtube.com/embed/KOzahYKJaZI?modestbranding=1&showinfo=0&controls=0&theme=light"
+    src="https://www.youtube.com/embed/oAlLond17UY?modestbranding=1&showinfo=0&controls=0&theme=light"
     title="Explanation video about Ledgy’s PSOP template guide"
   />
 );
