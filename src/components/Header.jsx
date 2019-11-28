ffc09d9702ed7126eaa4c5491bde86d335833a16
Jahlela Hasle <jahlela5@gmail.com>
2019-11-28 10:27:28 +0100

    Make postId dynamic for ProductHunt button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -73,6 +73,7 @@ export const CalculatorHeader = ({ data }: { data: any }) => {
                 <ProductHuntButton
                   productHuntLink="https://www.producthunt.com/posts/startup-fundraising-calculator?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-startup-fundraising-calculator"
                   trackSignupKey="clickProductHunt"
+                  postId="169181"
                   altText="Startup Fundraising Calculator - The calculator you need if you're raising money. | Product Hunt Embed"
                 />
               </div>
