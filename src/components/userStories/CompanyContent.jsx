53f90765c54d3ab00c390ee98c134300ad937656
Marius Colacioiu <marius.colacioiu@gmail.com>
2020-01-28 16:49:29 +0100

    Remove some not needed style, reuse bootstrap classes

--- src/components/userStories/CompanyContent.jsx
+++ src/components/userStories/CompanyContent.jsx
@@ -12,35 +12,33 @@ import { MarkdownContent } from '../MarkdownContent';
 export const CompanyContent = ({ company }: {| company: Company |}) => {
   console.log({ company });
   return (
-    <div className="company-content">
-      <div className="m-5">
-        <Img className="fit-cover mx-auto company-content-logo" {...company.logo} />
-        <div className="container">
-          <MarkdownContent content={company.mainQuote} className="company-content-quote blockquote" />
-          <div className="company-content-contact mb-2">
-            <div className="row mx-auto"><strong>{company.contactName}</strong></div>
-            <div className="row mx-auto">{company.contactTitle}</div>
-          </div>
-          <div className="row mx-auto py-5">
-            <FontAwesomeIcon fixedWidth size="lg" icon={faClock} title="Founded year" className="mr-4" />
-            Founded in {company.yearFounded}
-          </div>
-          <div className="row mx-auto py-5">
-            <FontAwesomeIcon fixedWidth size="lg" icon={faMoneyBillAlt} title="Funding" className="mr-4" />
-            {company.funding} in Funding
-          </div>
-          <div className="row mx-auto py-5">
-            <FontAwesomeIcon fixedWidth size="lg" icon={faUserFriends} title="Employees count" className="mr-4" />
-            {company.employeeCount} Employees
-          </div>
-          <div className="row mx-auto py-5">
-            <FontAwesomeIcon fixedWidth size="lg" icon={faIndustry} title="Sector" className="mr-4" />
-            {company.sector}
-          </div>
-          <div className="row mx-auto py-5">
-            <FontAwesomeIcon fixedWidth size="lg" icon={faSeedling} title="Stage" className="mr-4" />
-            {company.stage}
-          </div>
+    <div className="company-content p-5">
+      <Img className="fit-cover mx-auto company-content-logo" {...company.logo} />
+      <div className="container">
+        <MarkdownContent content={company.mainQuote} className="mb-0 font-italic blockquote" />
+        <div className="company-content-contact mb-2">
+          <div className="row mx-auto"><strong>{company.contactName}</strong></div>
+          <div className="row mx-auto">{company.contactTitle}</div>
+        </div>
+        <div className="row mx-auto py-5">
+          <FontAwesomeIcon fixedWidth size="lg" icon={faClock} title="Founded year" className="mr-4" />
+          Founded in {company.yearFounded}
+        </div>
+        <div className="row mx-auto py-5">
+          <FontAwesomeIcon fixedWidth size="lg" icon={faMoneyBillAlt} title="Funding" className="mr-4" />
+          {company.funding} in Funding
+        </div>
+        <div className="row mx-auto py-5">
+          <FontAwesomeIcon fixedWidth size="lg" icon={faUserFriends} title="Employees count" className="mr-4" />
+          {company.employeeCount} Employees
+        </div>
+        <div className="row mx-auto py-5">
+          <FontAwesomeIcon fixedWidth size="lg" icon={faIndustry} title="Sector" className="mr-4" />
+          {company.sector}
+        </div>
+        <div className="row mx-auto py-5">
+          <FontAwesomeIcon fixedWidth size="lg" icon={faSeedling} title="Stage" className="mr-4" />
+          {company.stage}
         </div>
       </div>
     </div>
