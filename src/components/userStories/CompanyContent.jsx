370ab62ab0a35a9bcd0e1ccb2a7c92172887c923
Marius Colacioiu <marius.colacioiu@gmail.com>
2020-01-28 16:09:56 +0100

    Add icons and more styling of company content section

--- src/components/userStories/CompanyContent.jsx
+++ src/components/userStories/CompanyContent.jsx
@@ -3,7 +3,7 @@
 import Img from 'gatsby-image';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faMoneyBillAlt, faUserFriends, faIndustry, faSeedling } from '@fortawesome/free-solid-svg-icons';
 import { Trans } from '@lingui/react';
 import sampleSize from 'lodash/sampleSize';
 
@@ -13,34 +13,34 @@ export const CompanyContent = ({ company }: {| company: Company |}) => {
   console.log({ company });
   return (
     <div className="company-content">
-      <Img className="fit-cover mx-auto company-content-logo" {...company.logo} />
-      <div className="container">
-        <div className="row mx-1">
+      <div className="m-5">
+        <Img className="fit-cover mx-auto company-content-logo" {...company.logo} />
+        <div className="container">
           <MarkdownContent content={company.mainQuote} className="company-content-quote blockquote" />
-        </div>
-        <div className="company-content-contact mb-2">
-          <div className="row mx-auto"><strong>{company.contactName}</strong></div>
-          <div className="row mx-auto">{company.contactTitle}</div>
-        </div>
-        <div className="row mx-auto py-4">
-          <FontAwesomeIcon icon={faClock} title="Founded year" className="mr-2" />
-          Founded in {company.yearFounded}
-        </div>
-        <div className="row mx-auto py-4">
-          <FontAwesomeIcon icon={faClock} title="Funding" className="mr-2" />
-          {company.funding} in Funding
-        </div>
-        <div className="row mx-auto py-4">
-          <FontAwesomeIcon icon={faClock} title="Employees count" className="mr-2" />
-          {company.employeeCount} Employees
-        </div>
-        <div className="row mx-auto py-4">
-          <FontAwesomeIcon icon={faClock} title="Sector" className="mr-2" />
-          {company.sector}
-        </div>
-        <div className="row mx-auto py-4">
-          <FontAwesomeIcon icon={faClock} title="Stage" className="mr-2" />
-          {company.stage}
+          <div className="company-content-contact mb-2">
+            <div className="row mx-auto"><strong>{company.contactName}</strong></div>
+            <div className="row mx-auto">{company.contactTitle}</div>
+          </div>
+          <div className="row mx-auto py-4">
+            <FontAwesomeIcon fixedWidth icon={faClock} title="Founded year" className="mt-1 mr-2" />
+            Founded in {company.yearFounded}
+          </div>
+          <div className="row mx-auto py-4">
+            <FontAwesomeIcon fixedWidth icon={faMoneyBillAlt} title="Funding" className="mt-1 mr-2" />
+            {company.funding} in Funding
+          </div>
+          <div className="row mx-auto py-4">
+            <FontAwesomeIcon fixedWidth icon={faUserFriends} title="Employees count" className="mt-1 mr-2" />
+            {company.employeeCount} Employees
+          </div>
+          <div className="row mx-auto py-4">
+            <FontAwesomeIcon fixedWidth icon={faIndustry} title="Sector" className="mt-1 mr-2" />
+            {company.sector}
+          </div>
+          <div className="row mx-auto py-4">
+            <FontAwesomeIcon fixedWidth icon={faSeedling} title="Stage" className="mt-1 mr-2" />
+            {company.stage}
+          </div>
         </div>
       </div>
     </div>
