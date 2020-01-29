d30c9451bb073a7e7f40185ebd1c6bbad84f930c
Marius Colacioiu <marius.colacioiu@gmail.com>
2020-01-29 12:21:27 +0100

    Fix style issues

--- src/components/customerStories/CompanySummary.jsx
+++ src/components/customerStories/CompanySummary.jsx
@@ -13,17 +13,20 @@ import {
 
 import { LongText } from '../LongText';
 
-const getCompanySummaries = (company: Company) => ([
-  [faClock, "Founded year", `Founded in ${company.yearFounded}`],
-  [faMoneyBillAlt, "Funding", `${company.funding} in Funding`],
-  [faUserFriends, "Employees count", `${company.employeeCount} Employees`],
-  [faIndustry, "Sector", company.sector],
-  [faSeedling, "Stage", company.stage],
-]);
+const getCompanySummaries = (company: Company) => [
+  [faClock, 'Founded year', `Founded in ${company.yearFounded}`],
+  [faMoneyBillAlt, 'Funding', `${company.funding} in Funding`],
+  [faUserFriends, 'Employees count', `${company.employeeCount} Employees`],
+  [faIndustry, 'Sector', company.sector],
+  [faSeedling, 'Stage', company.stage]
+];
 
 export const CompanySummary = ({ company }: {| company: Company |}) => (
   <div className="company-summary rounded-md bg-white sticky-top p-5">
-    <Img className="company-summary-logo font-weight-light fit-cover mx-auto mb-3" {...company.logo} />
+    <Img
+      className="company-summary-logo font-weight-light fit-cover mx-auto mb-3"
+      {...company.logo}
+    />
     <div className="container">
       <LongText
         content={company.mainQuote}
@@ -36,18 +39,12 @@ export const CompanySummary = ({ company }: {| company: Company |}) => (
         </div>
         <div className="row mx-auto">{company.contactTitle}</div>
       </div>
-      {getCompanySummaries(company).map(([icon, iconTitle, description]) =>
+      {getCompanySummaries(company).map(([icon, iconTitle, description]) => (
         <div key={iconTitle} className="row mx-auto py-3">
-          <FontAwesomeIcon
-            fixedWidth
-            size="lg"
-            icon={icon}
-            title={iconTitle}
-            className="mr-4"
-          />
+          <FontAwesomeIcon fixedWidth size="lg" icon={icon} title={iconTitle} className="mr-4" />
           {description}
         </div>
-      )}
+      ))}
     </div>
   </div>
 );
