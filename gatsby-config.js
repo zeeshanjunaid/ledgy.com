1f9598ce8eb789a46bfeb969d4b0a0257332c884
Timo Horstschäfer <mail@timo-horstschaefer.de>
2020-02-03 16:37:40 +0100

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     'gatsby-plugin-flow',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-nprogress',
     'gatsby-plugin-sitemap',
