99a078dd6efbc1c24618d89f7c3adac58beb6d50
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-02-11 12:19:42 +0100

    fix responsiveness SellingProp

--- src/components/SellingProp.jsx
+++ src/components/SellingProp.jsx
@@ -22,8 +22,8 @@ export const SellingProp = ({
   imgFirst?: boolean
 |}) => (
   <div className="container text-center p-4 p-lg-7 my-7">
-    <div className="row justify-content-center align-items-start">
-      <div className="col-12 col-md-6 d-flex flex-column text-left">
+    <div className="row justify-content-center align-items-center align-items-lg-start">
+      <div className="col-12 col-md-7 col-lg-6 d-flex flex-column text-left">
         <h2 className="custom-underline">{title}</h2>
         <p className="my-4">{subtitle}</p>
         <Link href to={link.to} className="d-flex align-items-center">
@@ -31,7 +31,9 @@ export const SellingProp = ({
           <span className="deco-link ml-2">{link.text}</span>
         </Link>
       </div>
-      <div className={`col-12 col-md-6 py-3 px-2 px-md-5 ${imgFirst ? 'order-md-first' : ''}`}>
+      <div
+        className={`col-12 col-md-5 col-lg-6 py-3 px-2 px-md-5 ${imgFirst ? 'order-md-first' : ''}`}
+      >
         <Img {...imgProps} />
       </div>
     </div>
