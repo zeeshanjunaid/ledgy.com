19707e634cbdbe6fb557888c1dfa09c2982e2125
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-02-14 12:03:52 +0100

    style Feature elements

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -18,15 +18,15 @@ export const Feature = ({
     <div className="container text-center p-4 p-lg-6 my-7">
       <div className="row justify-content-center align-items-center">
         <div className="col-12 col-md-7 col-lg-6 d-flex flex-column text-left">
-          <h2>{title}</h2>
-          <ul>
+          <h2 className="feature-title-decoration mb-3">{title}</h2>
+          <ul className="p-0 list-style-none">
             {description.map(point => (
-              <li>{point}</li>
+              <li className="feature-list-item-decoration mb-1">{point}</li>
             ))}
           </ul>
         </div>
         <div
-          className={`col-12 col-md-5 col-lg-6 py-3 px-2 px-md-5 ${
+          className={`col-12 col-md-5 col-lg-6 py-3 px-2 px-md-4 ${
             imgFirst ? 'order-md-first' : ''
           }`}
         >
