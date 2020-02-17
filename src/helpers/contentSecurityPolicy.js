30ab0422207785afa3d2ca46cc5dc2d90986db2c
Timo Horstschaefer <mail@timo-horstschaefer.de>
2020-02-17 18:33:59 +0100

    add some more resources from Sentry reports

--- src/helpers/contentSecurityPolicy.js
+++ src/helpers/contentSecurityPolicy.js
@@ -22,7 +22,7 @@ const defaultPolicy = {
   script: "'self' 'unsafe-inline' 'unsafe-eval'",
   style: "'self' 'unsafe-inline' https://fonts.googleapis.com",
   frame: "'self' https://www.youtube.com",
-  connect: "'self' ",
+  connect: "'self' https://fonts.googleapis.com",
   child: "'self'"
 };
 
@@ -35,7 +35,8 @@ const maps = {
   script: 'https://maps.googleapis.com',
   style: 'https://maps.googleapis.com',
   connect: 'https://maps.gstatic.com https://maps.googleapis.com',
-  img: 'https://maps.gstatic.com'
+  img: 'https://maps.gstatic.com https://maps.googleapis.com',
+  prefetch: 'https://maps.googleapis.com'
 };
 
 const segment = {
@@ -48,26 +49,30 @@ const googleAnalytics = {
     'https://www.google-analytics.com https://ssl.google-analytics.com https://snap.licdn.com https://www.googletagmanager.com https://www.googleadservices.com https://www.google.com https://googleads.g.doubleclick.net',
   frame: 'https://bid.g.doubleclick.net',
   connect:
-    'https://www.google-analytics.com https://www.googleadservices.com https://snap.licdn.com',
+    'https://www.google-analytics.com https://www.googleadservices.com https://www.google.com https://snap.licdn.com',
   img:
-    'http://www.google-analytics.com https://www.googletagmanager.com https://px.ads.linkedin.com https://www.google.com https://www.google.ch https://googleads.g.doubleclick.net https://www.google.com'
+    'http://www.google-analytics.com https://www.googletagmanager.com https://www.linkedin.com https://px.ads.linkedin.com https://www.google.com https://googleads.g.doubleclick.net https://www.google.ch https://www.google.com https://www.google.de https://www.google.fr',
+  prefetch:
+    'https://www.google-analytics.com https://www.googletagmanager.com https://js.hsadspixel.net'
 };
 
 const hubspot = {
   img: 'https://forms.hsforms.com https://track.hubspot.com',
   script:
-    'https://js.hs-scripts.com https://js.usemessages.com https://js.hsadspixel.net http://js.hs-analytics.net https://js.hscollectedforms.net https://js.hs-scripts.com',
+    'https://js.hs-scripts.com https://js.usemessages.com https://js.hsadspixel.net https://js.hs-analytics.net https://js.hscollectedforms.net https://js.hs-scripts.com',
   frame: 'https://app.hubspot.com',
   connect:
-    'https://api.hubspot.com https://forms.hubspot.com https://api.hubapi.com https://js.hs-scripts.com https://js.hsadspixel.net https://js.hs-analytics.net https://js.hscollectedforms.net https://js.usemessages.com'
+    'https://api.hubspot.com https://forms.hubspot.com https://api.hubapi.com https://js.hs-scripts.com https://js.hsadspixel.net https://js.hs-analytics.net https://js.hscollectedforms.net https://js.usemessages.com',
+  prefetch: 'https://js.hs-scripts.com https://js.hs-analytics.net https://script.hotjar.com'
 };
 
 const hotjar = {
   script: 'https://static.hotjar.com https://script.hotjar.com',
   frame: 'https://vars.hotjar.com',
-  connect: 'http://*.hotjar.com:* https://*.hotjar.com:* https://vc.hotjar.io:* wss://*.hotjar.com',
+  connect: 'https://*.hotjar.com:* https://vc.hotjar.io:* wss://*.hotjar.com',
   font: 'https://script.hotjar.com',
-  img: 'https://script.hotjar.com'
+  img: 'https://script.hotjar.com',
+  prefetch: 'https://static.hotjar.com'
 };
 
 exports.ContentSecurityPolicy = mergePolicies([
