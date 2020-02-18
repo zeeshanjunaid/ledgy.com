880ca18f86e5f3788e52e2001a5e9c586f746f8b
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-02-18 18:10:19 +0100

    fix navbar buttons

--- src/components/NavbarButtons.jsx
+++ src/components/NavbarButtons.jsx
@@ -9,12 +9,17 @@ import { Button } from './Button';
 
 export const NavbarButtons = ({ className = '' }: { className?: string }) => (
   <div className={`d-flex align-items-center ${className}`}>
-    <Button outline href={`${appUrl}/login`} className="p-2" onClick={() => track('click.login')}>
+    <Button
+      outline
+      href={`${appUrl}/login`}
+      className="px-3 py-1"
+      onClick={() => track('click.login')}
+    >
       <Trans>Log In</Trans>
     </Button>
     <Button
       inverted
-      className="ml-2 p-2"
+      className="ml-2 px-3 py-1"
       href={`${appUrl}/signup`}
       onClick={() => track('click.signup')}
     >
