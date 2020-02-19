ecdbca185ed053150d453002a64d830c613f0776
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-02-19 12:28:14 +0100

    update contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,101 +4,60 @@ import React from 'react';
 import { withI18n, Trans } from '@lingui/react';
 
 import { Title } from '../layouts/utils';
-
-const Header = ({ i18n }: Props) => (
-  <header className="header text-white">
-    <Title
-      title={i18n.t`Contact`}
-      description={i18n.t`Contact the Ledgy team by phone, email, or mailing address to learn more about cap table management, equity plans, and investor relations`}
-    />
-
-    <div className="container text-center">
-      <div className="row">
-        <div className="col-12 col-lg-8 offset-lg-2">
-          <h1>
-            <Trans>Let’s Get In Touch</Trans>
-          </h1>
-          <p>
-            <Trans>Here are the ways you can contact us with any questions you have</Trans>
-          </p>
-        </div>
-      </div>
-    </div>
-  </header>
-);
-
-class Map extends React.Component<{}> {
-  componentDidMount() {
-    if (window.page && window.google) {
-      window.page.initMap();
-    }
-  }
-  render() {
-    return (
-      <div
-        id="map"
-        className="h-400"
-        data-provide="map"
-        data-lat="47.3627638"
-        data-lng="8.5579592"
-        data-marker-lat="47.3627638"
-        data-marker-lng="8.5579592"
-        data-zoom="14"
-      />
-    );
-  }
-}
+import { DefaultHeader } from '../components/Header';
 
 const IndexPage = (props: Object) => (
   <div>
-    <Header {...props} />
+    <Title
+      title={props.i18n.t`Contact`}
+      description={props.i18n
+        .t`Contact the Ledgy team by phone, email, or mailing address to learn more about cap table management, equity plans, and investor relations`}
+    />
+    <DefaultHeader
+      title={<Trans>Let’s get in touch</Trans>}
+      subtitle={<Trans>Here are the ways you can contact us with any questions you have</Trans>}
+    />
 
-    <main className="main-content">
-      <div className="section">
-        <div className="container">
-          <div className="bg-gray h-full p-5 imprint">
-            <div className="row">
-              <div className="col-md-6">
-                <h6>Ledgy AG</h6>
-                <p>
-                  Forchstrasse 60
-                  <br />
-                  8008 Zürich
-                  <br />
-                  <Trans>Switzerland</Trans>
-                </p>
-                <h6>
-                  <Trans>VAT number</Trans>
-                </h6>
-                <p>CHE‑261.454.963 MWST</p>
-              </div>
-              <div className="col-md-6">
-                <h6>
-                  <Trans>Contact</Trans>
-                </h6>
-                <p>
-                  <a href="mailto:contact@ledgy.com">contact@ledgy.com</a>
-                  <br />
-                  +41 (0) 44 585 21 23
-                </p>
-                <h6>
-                  <Trans>Managing directors</Trans>
-                </h6>
-                <p>
-                  Yoko Spirig
-                  <br />
-                  Ben-Elias Brandt
-                  <br />
-                  Timo Horstschäfer
-                </p>
-              </div>
-            </div>
+    <div className="container">
+      <div className="bg-gray h-full p-5 imprint">
+        <div className="row">
+          <div className="col-md-6">
+            <h6>Ledgy AG</h6>
+            <p>
+              Forchstrasse 60
+              <br />
+              8008 Zürich
+              <br />
+              <Trans>Switzerland</Trans>
+            </p>
+            <h6>
+              <Trans>VAT number</Trans>
+            </h6>
+            <p>CHE‑261.454.963 MWST</p>
+          </div>
+          <div className="col-md-6">
+            <h6>
+              <Trans>Contact</Trans>
+            </h6>
+            <p>
+              <a href="mailto:contact@ledgy.com">contact@ledgy.com</a>
+              <br />
+              +41 (0) 44 585 21 23
+            </p>
+            <h6>
+              <Trans>Managing directors</Trans>
+            </h6>
+            <p>
+              Yoko Spirig
+              <br />
+              Ben-Elias Brandt
+              <br />
+              Timo Horstschäfer
+            </p>
           </div>
         </div>
       </div>
-
-      <Map />
-    </main>
+    </div>
   </div>
 );
 
