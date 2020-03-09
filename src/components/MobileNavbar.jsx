cd28d66262292a4a33acaf1497918ea28fee0248
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-03-09 18:20:13 +0100

    dirty fix for mobile nav scrolling on iOS

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -56,22 +56,24 @@ export const MobileNavbar = ({
   toggleOverlay: (boolean, (boolean) => void) => void,
   prefix: string
 |}) => (
-  <div className="mobile-navbar position-fixed text-primary">
-    <div className="d-flex flex-column align-items-center justify-content-center p-2 p-sm-4 mt-3 mt-sm-4">
-      <div className="text-left ml-n2 ml-sm-n4">
-        {titlesAndLinks.map(([title, links], i) => (
-          <MobileNavbarGroup
-            title={title}
-            links={links}
-            prefix={prefix}
-            isOpen={isOpen}
-            setOpen={setOpen}
-            toggleOverlay={toggleOverlay}
-            key={i} // eslint-disable-line react/no-array-index-key
-          />
-        ))}
+  <div id="mobile-navbar" className="position-fixed text-primary">
+    <div className="mobile-inner">
+      <div className="d-flex flex-column align-items-center justify-content-center p-2 p-sm-4 mt-3 mt-sm-4">
+        <div className="text-left ml-n2 ml-sm-n4">
+          {titlesAndLinks.map(([title, links], i) => (
+            <MobileNavbarGroup
+              title={title}
+              links={links}
+              prefix={prefix}
+              isOpen={isOpen}
+              setOpen={setOpen}
+              toggleOverlay={toggleOverlay}
+              key={i} // eslint-disable-line react/no-array-index-key
+            />
+          ))}
+        </div>
+        <NavbarButtons className="mt-2 mt-sm-4 mb-4" isMobile />
       </div>
-      <NavbarButtons className="mt-2 mt-sm-4 mb-4" isMobile />
     </div>
   </div>
 );
