dd855dcd9c5200f7c4965b7470f3ce17fee39410
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-03-10 14:40:33 +0100

    fix strange header merge behavior

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 
 export default () => (
-  <header className="header loader-banner d-flex justify-content-center align-items-center">
-    <FontAwesomeIcon icon={faCircleNotch} />
-  </header>
+  <section>
+    <header className="loader-banner d-flex justify-content-center align-items-center">
+      <FontAwesomeIcon icon={faCircleNotch} />
+    </header>
+  </section>
 );
