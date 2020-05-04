adabab70c379419e2e8395c90b707e759c0e28fc
Timo Horstschaefer <mail@timo-horstschaefer.de>
2020-05-04 12:44:17 +0200

    indent

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -87,15 +87,15 @@ const TemplateWrapper = withI18n()(({ children, ...props }: SiteProps) => (
             <link rel="alternate" href={`${siteUrl}/fr${pathname}`} hrefLang="fr" />
             <script>
               {`
-              (function (c, p, d, u, id, i) {
-                id = ''; // Optional Custom ID for user in your system
-                u = 'https://tracking.g2crowd.com/attribution_tracking/conversions/' + c + '.js?p=' + encodeURI(p) + '&e=' + id;
-                i = document.createElement('script');
-                i.type = 'application/javascript';
-                i.async = true;
-                i.src = u;
-                d.getElementsByTagName('head')[0].appendChild(i);
-              }("3729", document.location.href, document));
+                (function (c, p, d, u, id, i) {
+                  id = ''; // Optional Custom ID for user in your system
+                  u = 'https://tracking.g2crowd.com/attribution_tracking/conversions/' + c + '.js?p=' + encodeURI(p) + '&e=' + id;
+                  i = document.createElement('script');
+                  i.type = 'application/javascript';
+                  i.async = true;
+                  i.src = u;
+                  d.getElementsByTagName('head')[0].appendChild(i);
+                }("3729", document.location.href, document));
               `}
             </script>
 
