c6a33d7a10134e3698adc6bc41ed72edb3f9194c
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-10-13 17:48:13 +0200

    add dynamic translations to demo page

--- src/layouts/demo.jsx
+++ src/layouts/demo.jsx
@@ -4,12 +4,14 @@ import { Link, graphql } from 'gatsby';
 import { withI18n } from '@lingui/react';
 import React from 'react';
 
-import { Title } from './utils';
 import { DemoForm } from '../components/forms';
 import { ExternalLogoRow } from '../components/ExternalLogoRow';
 import { SellingProp } from '../components/SellingProp';
 import { CTABanner } from '../components/CTABanner';
+import { dynamicI18n, DynamicTrans } from '../components/DynamicTrans';
 import logoInvertedCompact from '../img/logo-inverted-compact.png';
+
+import { Title } from './utils';
 
 const Logo = (props: { prefix: string }) => (
   <Link href to={`${props.prefix}/#start`}>
@@ -32,7 +34,7 @@ const DecoShapes = () => (
 );
 
 const DemoPage = (props: LayoutProps) => {
-  const { data, prefix, location } = props;
+  const { data, prefix, location, i18n } = props;
   const {
     title,
     description,
@@ -41,19 +43,21 @@ const DemoPage = (props: LayoutProps) => {
     content,
     type,
   } = data.contentfulSignupPage; // TODO rename in Contentful
-
+  const t = dynamicI18n(i18n);
   return (
     <>
-      <Title title={title} description={description} />
+      <Title title={t(title)} description={t(description)} />
       <header className="header d-flex home-banner px-1 text-left bg-primary overflow-hidden">
         <div className="container my-4 my-md-auto position-relative z-index-base">
           <div className="row mt-4 mt-lg-2 pb-4 pb-md-6">
             <div className="col-lg-6 d-flex flex-column justify-content-center">
               <div className="mt-lg-n4 mb-md-4 mr-md-4">
                 <Logo {...props} />
-                <h1 className="text-white mt-5 mb-2 mb-sm-3">{title}</h1>
+                <h1 className="text-white mt-5 mb-2 mb-sm-3">
+                  <DynamicTrans>title</DynamicTrans>
+                </h1>
                 <div className="text-lg line-height-lg text-white font-weight-light pb-3">
-                  {description}
+                  <DynamicTrans>{description}</DynamicTrans>
                 </div>
               </div>
             </div>
