74fa9262199e656a6d5e0eb5b6a78bbfa08f6762
Oriol VidalCortes <ovidalcortes@gmail.com>
2020-10-13 18:10:27 +0200

    have CTA banner scroll to top in demo page

--- src/components/CTABanner.jsx
+++ src/components/CTABanner.jsx
@@ -8,11 +8,23 @@ import { callToActionExperiments } from '../layouts/utils';
 import { isBrowser } from '../helpers';
 import { RequestDemoButton } from './RequestDemoButton';
 
-const Banner = ({ title, prefix }: {| title: Node, prefix: string |}) => (
+const Banner = ({
+  title,
+  prefix,
+  location,
+}: {|
+  title: Node,
+  prefix: string,
+  location?: LocationProps,
+|}) => (
   <div className="container p-4 p-lg-7 my-7">
     <div className="row m-0 w-100 flex-column justify-content-center align-items-center">
       <h3 className="mb-4 text-center font-weight-bold">{title}</h3>
-      <RequestDemoButton buttonClassName="btn-red text-lg px-4 py-2" prefix={prefix} />
+      <RequestDemoButton
+        buttonClassName="btn-red text-lg px-4 py-2"
+        prefix={prefix}
+        location={location}
+      />
     </div>
   </div>
 );
@@ -25,5 +37,5 @@ export const CTABanner = ({ location, prefix }: LayoutProps) => {
   }
 
   const experiment = isBrowser ? sample(callToActionExperiments) : callToActionExperiments[0];
-  return <Banner title={experiment.title} prefix={prefix} />;
+  return <Banner title={experiment.title} prefix={prefix} location={location} />;
 };
--- src/components/RequestDemoButton.jsx
+++ src/components/RequestDemoButton.jsx
@@ -9,15 +9,36 @@ import { Button } from './Button';
 export const RequestDemoButton = ({
   buttonClassName,
   prefix,
+  location,
 }: {|
   buttonClassName: string,
   prefix: string,
-|}) => (
-  <>
-    <Link href to={`${prefix}/demo/ledgy`}>
-      <Button cta className={buttonClassName}>
-        <Trans>Get a Demo</Trans>
-      </Button>
-    </Link>
-  </>
-);
+  location?: LocationProps,
+|}) => {
+  const isDemoPage = !!location && location.pathname.includes('demo');
+  const ButtonText = <Trans>Get a Demo</Trans>;
+
+  return isDemoPage ? (
+    <Button
+      cta
+      className={buttonClassName}
+      onClick={() =>
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth',
+        })
+      }
+    >
+      {ButtonText}
+    </Button>
+  ) : (
+    <>
+      <Link href to={`${prefix}/demo/ledgy`}>
+        <Button cta className={buttonClassName}>
+          {ButtonText}
+        </Button>
+      </Link>
+    </>
+  );
+};
