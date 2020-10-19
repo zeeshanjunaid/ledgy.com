3694970ff91cb20437d17edc121e4ed7bb992518
Timo Horstschaefer <mail@timo-horstschaefer.de>
2020-10-19 12:37:00 +0200

    add small investor handling

--- src/components/forms/lib/constants.js
+++ src/components/forms/lib/constants.js
@@ -8,6 +8,7 @@ export const INVESTOR: RequesterType = 'investor';
 
 export const SMALL_COMPANY_THRESHOLD = 10;
 export const DEER_COMPANY_THRESHOLD = 50;
+export const FUND_INVESTMENT_THRESHOLD = 10;
 
 export const PREMIUM_EMPLOYEE_VALUE = 2;
 export const ENTERPRISE_EMPLOYEE_VALUE = 4;
@@ -15,4 +16,5 @@ export const INVESTMENT_VALUE = 15;
 
 export const smallCompanyUrl = demoUrl;
 export const deerCompanyUrl = 'https://meetings.hubspot.com/ledgy/equity-management-with-ledgy';
-export const investorUrl = 'https://meetings.hubspot.com/ledgy/portfolio-management-with-ledgy';
+export const investorUrl = 'https://demo.ledgy.com/3whvEG3wR6qMYGnyy/dashboard';
+export const fundUrl = 'https://meetings.hubspot.com/ledgy/portfolio-management-with-ledgy';
