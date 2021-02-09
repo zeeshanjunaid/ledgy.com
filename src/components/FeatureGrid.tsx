c11390459839f8f7bd4ce4e0c3bd6ca48b7c1cea
Oriol VidalCortes <ovidalcortes@gmail.com>
2021-02-09 11:12:30 +0100

    responsiveness fixes

--- src/components/FeatureGrid.tsx
+++ src/components/FeatureGrid.tsx
@@ -10,8 +10,8 @@ const FeatureGridCard = ({
 }) => {
   const { title, description, icon } = featureGridSection;
   return (
-    <div className="col-12 col-sm-6 col-xl-3">
-      <div className="left-border-with-accent">
+    <div className="col-12 col-sm-6 col-xl-3 mb-4 mb-xl-0">
+      <div className="left-border-with-accent h-100">
         <Icon icon={icon} />
         <h5 className="my-2 accent-border">
           <DynamicTrans>{title}</DynamicTrans>
@@ -35,7 +35,7 @@ export const FeatureGrid = ({
       <h2 className="text-left text-weight-bold mb-5">
         <DynamicTrans>{header}</DynamicTrans>
       </h2>
-      <div className="row my-4 align-top">
+      <div className="row my-4">
         {sections.map((v) => (
           <FeatureGridCard key={v.title} featureGridSection={v} />
         ))}
