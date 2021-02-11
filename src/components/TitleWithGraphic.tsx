b72059e148ee76873962dbe1ab8a066885a58ef1
Xiao Sun <xiao@ledgy.com>
2021-02-11 18:04:37 +0100

    mirrored

--- src/components/TitleWithGraphic.tsx
+++ src/components/TitleWithGraphic.tsx
@@ -10,23 +10,26 @@ export const TitleWithGraphic = ({
   graphic,
   description,
   inverted = false,
-}: TitleWithGraphicProps & { inverted?: boolean }) => {
+  mirrored = false,
+}: TitleWithGraphicProps & { inverted?: boolean; mirrored?: boolean }) => {
   const { childImageSharp } = graphic?.localFile || {};
   const sectionBackgroundStyle = inverted ? 'bg-lightest' : '';
   const backgroundStyle = inverted ? '' : 'bg-primary tilted-background';
   const motivationTextColor = inverted ? 'text-gray-dark' : 'text-secondary';
   const titleTextColor = inverted ? 'text-primary' : 'text-secondary';
   const descriptionTextColor = inverted ? 'text-primary' : 'text-white';
+  const order = mirrored ? 'order-last' : '';
 
   return (
     <div className="overflow-hidden" style={{ margin: '-7rem 0', padding: '7rem 0' }}>
       <Section className={`position-relative my-7 ${sectionBackgroundStyle}`}>
         <div className={`${backgroundStyle} position-absolute z-index-background`}></div>
         <div className="row justify-content-center my-7 py-7">
-          <div className="col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end">
+          <div
+            className={`${order} col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end`}
+          >
             {!!childImageSharp && <Img {...childImageSharp} />}
           </div>
-
           <div className="col-lg-5">
             <p className={motivationTextColor}>
               <DynamicTrans>{motivationText.toUpperCase()}</DynamicTrans>
