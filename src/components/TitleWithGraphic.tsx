0a63aac4e7635ceeb2c3080ad39dd24afdf5b6bf
Oriol VidalCortes <ovidalcortes@gmail.com>
2021-02-13 20:58:30 +0100

    fix title with graphic

--- src/components/TitleWithGraphic.tsx
+++ src/components/TitleWithGraphic.tsx
@@ -13,26 +13,28 @@ export const TitleWithGraphic = ({
   const { childImageSharp } = graphic?.localFile || {};
 
   return (
-    <Section className="position-relative my-7">
-      <div className="bg-primary position-absolute tilted-background z-index-background"></div>
-      <div className="row justify-content-center my-7 py-7">
-        <div className="col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end">
-          {!!childImageSharp && <Img {...childImageSharp} />}
-        </div>
+    <div className="overflow-hidden" style={{ margin: '-7rem 0', padding: '7rem 0' }}>
+      <Section className="position-relative my-7">
+        <div className="bg-primary position-absolute tilted-background z-index-background"></div>
+        <div className="row justify-content-center my-7 py-7">
+          <div className="col-lg-4 d-flex align-items-center justify-content-center justify-content-lg-end">
+            {!!childImageSharp && <Img {...childImageSharp} />}
+          </div>
 
-        <div className="col-lg-5">
-          <p className="text-secondary">
-            <DynamicTrans>{motivationText.toUpperCase()}</DynamicTrans>
-          </p>
-          <h1
-            className="custom-underline my-4 font-weight-bold text-secondary"
-            dangerouslySetInnerHTML={{ __html: getUnderlineHtml(dynamicI18n(title)) }}
-          />
-          <p className="text-white">
-            <DynamicTrans>{description}</DynamicTrans>
-          </p>
+          <div className="col-lg-5">
+            <p className="text-secondary">
+              <DynamicTrans>{motivationText.toUpperCase()}</DynamicTrans>
+            </p>
+            <h1
+              className="custom-underline my-4 font-weight-bold text-secondary"
+              dangerouslySetInnerHTML={{ __html: getUnderlineHtml(dynamicI18n(title)) }}
+            />
+            <p className="text-white">
+              <DynamicTrans>{description}</DynamicTrans>
+            </p>
+          </div>
         </div>
-      </div>
-    </Section>
+      </Section>
+    </div>
   );
 };
