01c66d9cfea74bf3b9d060576ea8c2d56ec33bef
Xiao Sun <xiao@ledgy.com>
2021-03-19 15:19:39 +0100

    add link for catarina and marina

--- src/layouts/team.tsx
+++ src/layouts/team.tsx
@@ -214,6 +214,7 @@ export const getWholeTeam = (
     ),
     linkedIn: 'https://www.linkedin.com/in/marinamoric/',
     mail: 'marina@ledgy.com',
+    article: 'marina-moric',
   },
   catarina: {
     name: 'Catarina Simoes',
@@ -227,6 +228,7 @@ export const getWholeTeam = (
     ),
     linkedIn: 'https://www.linkedin.com/in/catarina-martins-sim%C3%B5es-62b8b9142/',
     mail: 'catarina@ledgy.com',
+    article: 'catarina-simoes',
   },
   nicolas: {
     name: 'Nicolas Briner',
