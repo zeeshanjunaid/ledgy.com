164d493d7bdbddada307efca0d575478fb59467a
Xiao Sun <xiao@ledgy.com>
2021-04-23 09:46:37 +0000

    fix typo

--- src/components/LongTextBuildingBlock.tsx
+++ src/components/LongTextBuildingBlock.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Section } from './utils';
 import { LongText } from './markdown';
+import { Section } from './utils';
 
 export const LongTextBuildingBlock = ({
   width,
   longTextContent,
   prefix,
 }: { prefix: string } & LongTextBuildingBlockProps) => {
-  const isWideMarkdown = width === 'wide';
+  const isWideMarkdown = width === 'Wide';
   return (
     <Section>
       <LongText content={longTextContent} prefix={prefix} isWideMarkdown={isWideMarkdown} />
