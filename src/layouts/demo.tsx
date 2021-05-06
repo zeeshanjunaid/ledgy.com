36df03188509324a803994b31748ee2338f2b780
Timo Horstschaefer <mail@timo-horstschaefer.de>
2021-05-06 17:45:10 +0200

    remove links from badges

--- src/layouts/demo.tsx
+++ src/layouts/demo.tsx
@@ -23,31 +23,19 @@ type DemoPageProps = {
 };
 
 const CapterraBadge = () => (
-  <a
-    href="https://www.capterra.com/reviews/173939/Ledgy?utm_source=vendor&utm_medium=badge&utm_campaign=capterra_reviews_badge"
-    className="mr-4"
-    {...targetBlank}
-  >
-    <img
-      className="capterra-badge d-none d-md-block"
-      src="https://assets.capterra.com/badge/4700aedd505fa5881254166d19949239.png?v=2120646&p=173939"
-      alt="Ledgy high Capterra rating"
-    />
-  </a>
+  <img
+    className="capterra-badge d-none d-md-block mr-4"
+    src="https://assets.capterra.com/badge/4700aedd505fa5881254166d19949239.png?v=2120646&p=173939"
+    alt="Ledgy high Capterra rating"
+  />
 );
 
 const G2Badge = () => (
-  <a
-    title="Ledgy is a leader in Equity Management on G2"
-    href="https://www.g2.com/products/ledgy/reviews?utm_source=rewards-badge"
-    {...targetBlank}
-  >
-    <img
-      className="g2-badge d-none d-md-block"
-      alt="Ledgy is a leader in Equity Management on G2"
-      src="https://images.g2crowd.com/uploads/report_medal/image/1335/medal.svg"
-    />
-  </a>
+  <img
+    className="g2-badge d-none d-md-block mr-4"
+    alt="Ledgy is a leader in Equity Management on G2"
+    src="https://images.g2crowd.com/uploads/report_medal/image/1335/medal.svg"
+  />
 );
 
 const DemoPage = (props: Props) => {
