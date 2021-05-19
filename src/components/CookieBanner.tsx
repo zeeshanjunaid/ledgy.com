020ab5e5301f0a6101abd8a441f6ed3b1a348cc8
Timo Horstschaefer <mail@timo-horstschaefer.de>
2021-05-19 16:25:59 +0200

    make cookie banner responsive

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -35,10 +35,10 @@ const Banner = ({
 }) => (
   <div className={bannerClassName}>
     <div className="m-4 row">
-      <div className="col-10 pl-xl-6 pl-2">
+      <div className="col-12 col-md-10 pl-xl-6 pl-2">
         <MDXRenderer>{content.childMdx.body}</MDXRenderer>
       </div>
-      <div className="col-2 d-flex align-items-center justify-content-center">
+      <div className="col-12 col-md-2 d-flex align-items-center justify-content-center">
         <ConfirmCookieButton hide={hide} acceptCookies={acceptCookies} />
       </div>
     </div>
