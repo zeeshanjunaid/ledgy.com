dbde63feeca19453a6de0bf89b49393ab4125822
Timo Horstschaefer <mail@timo-horstschaefer.de>
2021-05-28 12:12:22 +0200

    update pipeline value

--- src/components/forms/lib/constants.ts
+++ src/components/forms/lib/constants.ts
@@ -4,10 +4,10 @@ export const COMPANY: RequesterType = 'company';
 export const INVESTOR: RequesterType = 'investor';
 
 export const DEER_COMPANY_THRESHOLD = 60;
-export const FUND_INVESTMENT_THRESHOLD = 20;
+export const FUND_INVESTMENT_THRESHOLD = 10;
 
 export const EMPLOYEE_VALUE = 4;
-export const INVESTMENT_VALUE = 15;
+export const INVESTMENT_VALUE = 25;
 
 export const smallCompanyUrl = '/company-demo';
 export const investorUrl = '/investor-demo';
