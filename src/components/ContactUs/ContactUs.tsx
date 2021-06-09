28806dfbab81fe0e045e8b4dd647710b83c6f6bb
Xiao Sun <xiao@ledgy.com>
2021-06-09 15:58:00 +0200

    improve contact ui

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -6,10 +6,11 @@ import { ManagingDirectors } from './ManagingDirectors';
 export const ContactUs = () => (
   <div className="container bg-gray h-full p-5 imprint">
     <div className="row">
-      <div className="col-md-6">
+      <div className="col-md-1"></div>
+      <div className="col-md-5">
         <AddressAndVat />
       </div>
-      <div className="col-md-3">
+      <div className="col-md-6">
         <div className="d-flex flex-column flex-lg-row">
           <Emails />
           <ManagingDirectors />
