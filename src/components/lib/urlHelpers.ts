ecca6b8cd88c72d2e0b3b5eb5bec9381e7487684
Timo Horstschaefer <mail@timo-horstschaefer.de>
2021-06-10 18:14:23 +0200

    fix isExternalUrl

--- src/components/lib/urlHelpers.ts
+++ src/components/lib/urlHelpers.ts
@@ -1,5 +1,5 @@
 export const isExternalUrl = (url: string): boolean =>
-  url.startsWith('http:') || url.startsWith('www:') || url.startsWith('mailto:');
+  url.startsWith('http') || url.startsWith('www') || url.startsWith('mailto');
 
 const stripDoubleSlashes = (url: string): string => url.replace(/\/\//g, '/');
 export const formatUrl = (prefix: string, url: string): string =>
