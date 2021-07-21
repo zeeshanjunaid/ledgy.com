1911a0130b66fc1f8f4a5fd88c0e55edcc1334f4
Timo Horstschaefer <mail@timo-horstschaefer.de>
2021-07-21 12:13:23 +0200

    unset deprecated tag on Loco

--- build.sh
+++ build.sh
@@ -8,9 +8,11 @@ set -e
 npm rebuild sharp node-sass
 gatsby build --log-pages
 
+DEPRECATED_TAG="deprecated"
+
 if [ "$BRANCH" = "master" ]; then
   npm run extract
   cd src/locale/en
   cat dynamic.po >> messages.po
-  curl -u ${LOCO_KEY_REBRANDING}: --data-binary @messages.po https://localise.biz/api/import/po?tag-absent=deprecated
+  curl -u ${LOCO_KEY_REBRANDING}: --data-binary @messages.po "https://localise.biz/api/import/po?tag-absent=${DEPRECATED_TAG}&untag-all=${DEPRECATED_TAG}"
 fi
