deae6560779f0cffc83ff519b3f2c2de25f68f2f
Xiao Sun <xiao@ledgy.com>
2021-08-13 17:50:27 +0200

    adjust mobile view

--- src/components/utils/ButtonGroup.tsx
+++ src/components/utils/ButtonGroup.tsx
@@ -10,11 +10,11 @@ export const ButtonGroup = ({
   onClick: (arg: string) => void;
   tag: string;
 }) => (
-  <div className="d-flex justify-content-between mb-6">
+  <div className="d-flex justify-content-center justify-content-md-between flex-wrap mb-4 mb-md-6">
     {buttonTexts.map((buttonText) => (
-      <div key={buttonText}>
+      <div key={buttonText} className="p-1 p-md-0">
         <Button
-          className={`button-tag mb-4  ${tag === buttonText ? 'selected' : ''}`}
+          className={`button-tag mb-2  ${tag === buttonText ? 'selected' : ''}`}
           onClick={() => onClick(buttonText)}
         >
           <DynamicTrans>{buttonText}</DynamicTrans>
