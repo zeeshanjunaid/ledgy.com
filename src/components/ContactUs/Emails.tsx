4a208e2ca2d6591bb209850031b1ee82f8816504
Giacomo Venier <giacomo.venier@gmail.com>
2022-01-26 13:57:03 +0100

    remove legal@ledgy.com email address from website

--- src/components/ContactUs/Emails.tsx
+++ src/components/ContactUs/Emails.tsx
@@ -32,17 +32,11 @@ export const Emails = () => (
       </h6>
       <a href="mailto:work@ledgy.com">work@ledgy.com</a>
     </div>
-    <div className="mb-4">
+    <div>
       <h6>
         <Trans>General</Trans>
       </h6>
       <a href="mailto:contact@ledgy.com">contact@ledgy.com</a>
-    </div>
-    <div>
-      <h6>
-        <Trans>Legal</Trans>
-      </h6>
-      <a href="mailto:legal@ledgy.com">legal@ledgy.com</a>
     </div>
   </div>
 );
