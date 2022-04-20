f1ce641da90e3dd70b343144b764e69b7508fde2
VenierGiacomo <giacomo.venier@gmail.com>
2022-04-20 18:49:28 +0100

    lower company size threshold for demo

--- src/components/forms/lib/constants.ts
+++ src/components/forms/lib/constants.ts
@@ -6,7 +6,7 @@ export const INVESTOR: RequesterType = 'investor';
 export const NEWSLETTER: LeadFormType = 'newsletter';
 export const DEMO_REQUEST: LeadFormType = 'demoRequest';
 
-export const DEER_COMPANY_THRESHOLD = 60;
+export const DEER_COMPANY_THRESHOLD = 40;
 export const FUND_INVESTMENT_THRESHOLD = 10;
 
 export const EMPLOYEE_VALUE = 4;
