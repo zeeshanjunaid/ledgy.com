95c59768b15901692d1e7b3080523d76156c7ede
xiao <sunxiaolive2016@outlook.com>
2022-05-05 14:07:31 +0200

    add enterprisePlanButton

--- src/components/pricing/CompanyPricing.tsx
+++ src/components/pricing/CompanyPricing.tsx
@@ -17,7 +17,12 @@ export const CompanyPricing = ({ prefix }: Prefix) => (
           <DynamicTrans>Sign up</DynamicTrans>
         </Button>
       }
-      growthPlanText="7-day free trial"
+      enterprisePlanButton={
+        <Button outline inverted className="mt-4 mb-1" href={`${appUrl}/signup`}>
+          <DynamicTrans>Sign up</DynamicTrans>
+        </Button>
+      }
+      freeTrialText="7-day free trial"
       scalePlanButton={<RequestDemoButton prefix={prefix} buttonClassName="my-4 btn-red" />}
       highlightScalePlan
       DynamicTrans={DynamicTrans}
