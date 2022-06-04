4c5eb58b08bb1d62e525a963a96469e8b5576feb
VenierGiacomo <giacomo.venier@gmail.com>
2022-06-04 19:46:33 +0200

    fix prettier error

--- src/components/pricing/CompanyPricingCTAs.tsx
+++ src/components/pricing/CompanyPricingCTAs.tsx
@@ -16,7 +16,8 @@ const companyPricingCTAs: CompanyPricingCTAProps[] = [
     description: (
       <>
         <DynamicTrans>
-          You get a 20% discount if your startup helps reduce carbon emissions with its core business.
+          You get a 20% discount if your startup helps reduce carbon emissions with its core
+          business.
         </DynamicTrans>{' '}
         <a href="https://ble2zo3huer.typeform.com/to/g8yJMvoM">
           <DynamicTrans>Apply here!</DynamicTrans>
