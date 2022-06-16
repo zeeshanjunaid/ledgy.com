f13a4b8929689b387001ea16742b308cfd72d5a7
VenierGiacomo <giacomo.venier@gmail.com>
2022-06-16 09:06:05 +0200

    update productHunt url

--- src/components/productHuntButtons/PHEmployeeCompensationButton.tsx
+++ src/components/productHuntButtons/PHEmployeeCompensationButton.tsx
@@ -4,9 +4,9 @@ import { ProductHuntButton } from '../ProductHuntButton';
 
 export const PHEmployeeCompensationButton = () => (
   <ProductHuntButton
-    productHuntLink="https://www.producthunt.com/posts/startup-fundraising-calculator?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-startup-fundraising-calculator"
+    productHuntLink="https://www.producthunt.com/posts/the-full-compensation-calculator?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-the&#0045;full&#0045;compensation&#0045;calculator"
     trackSignupKey="clickProductHunt"
-    postId="169181"
-    altText="Startup Fundraising Calculator - The calculator you need if you're raising money. | Product Hunt Embed"
+    postId="349688"
+    altText="The&#0032;Full&#0032;Compensation&#0032;Calculator - Understand&#0032;the&#0032;value&#0032;of&#0032;your&#0032;new&#0032;job&#0032;offer | Product Hunt"
   />
 );
