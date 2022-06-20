03220380f9890ced49c7eeeeed4b76ce223b09c3
VenierGiacomo <giacomo.venier@gmail.com>
2022-06-20 14:55:41 +0200

    small ui changes

--- src/components/TestimonialsCarousel/LogosBar.tsx
+++ src/components/TestimonialsCarousel/LogosBar.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Image } from '../utils';
+import { scroller } from 'react-scroll';
 
 export const LogosBar = ({
   testimonials,
@@ -11,16 +12,30 @@ export const LogosBar = ({
   setActiveIndex: (index: number) => void;
   activeIndex: number;
 }) => {
+  useEffect(() => {
+    scroller.scrollTo('scrollTo', {
+      duration: 500,
+      delay: 0,
+      smooth: true,
+      horizontal: true,
+      containerId: 'scrollableLogoContainer',
+      offset: 0,
+      isDynamic: true,
+    });
+  }, [activeIndex]);
+
   return (
     <div className="fullheighttestimonial-logos">
-      <div className="fullheighttestimonial-logos-wrapper">
+      <div className="fullheighttestimonial-logos-wrapper" id="scrollableLogoContainer">
         {testimonials.map(({ logo }, index) => (
           <motion.div
             key={index}
             onClick={() => setActiveIndex(index)}
-            className={`fullheighttestimonial-logos-item `}
+            className={`fullheighttestimonial-logos-item ${
+              index === activeIndex - 1 ? 'scrollTo' : ''
+            }`}
             animate={{
-              opacity: index === activeIndex ? '1' : '.4',
+              opacity: index === activeIndex ? '1' : '0.4',
             }}
             initial={{ opacity: 0.4 }}
             transition={{ duration: 0.35, easeing: 'anticipate' }}
